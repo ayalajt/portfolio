90a2e31a74f40a38ee4656ba2d9bd20b4e773c9b
Jesus <ayalajt@vcu.edu>
2024-12-15 18:08:33 -0500

    Created Hamburger Menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,9 @@
 import { Component, Input, ViewEncapsulation, EventEmitter, Output } from '@angular/core';
+import { MenuComponent } from '../menu/menu.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MenuComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.less',
   encapsulation: ViewEncapsulation.None,
@@ -10,8 +11,17 @@ import { Component, Input, ViewEncapsulation, EventEmitter, Output } from '@angu
 export class HeaderComponent {
   @Input() applyIntroScrolled = false;
   @Output() toggleDarkMode = new EventEmitter<any>(); 
+  menuToggled = false;
 
   darkModeClick() {
     this.toggleDarkMode.emit();
+  }
+
+  menuClick() {
+    this.menuToggled = !this.menuToggled
+  }
+
+  closeMenu() {
+    this.menuToggled = false;
   }
 }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewEncapsulation, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   imports: [],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.less'
+  styleUrl: './menu.component.less',
+  encapsulation: ViewEncapsulation.None,
 })
-export class MenuComponent {
 
+export class MenuComponent {
+  @Input() menuOpened!: any;
+  @Output() emitCloseMenu = new EventEmitter<boolean>();
+
+  closeMenu() {
+    this.emitCloseMenu.emit(false);
+  }
 }

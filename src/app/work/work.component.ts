51a0a4bfedc96bb819e8e5fd0434c389e02f2b06
Jesus <ayalajt@vcu.edu>
2025-01-02 20:49:03 -0500

    Updated content

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -35,14 +35,18 @@ export class WorkComponent {
       storytelling and user engagement within articles.
       `,
       year: "2024",
-      link: "https://www.jnj.com/our-heritage/",
+      link: "https://www.jnj.com/our-heritage/our-beginning",
       images: [
         "/images/heritage/heritage-01.webp", "/images/heritage/heritage-02.webp", "/images/heritage/heritage-03.webp", "/images/heritage/heritage-04.webp", "/images/heritage/heritage-05.webp", "/images/heritage/heritage-06.webp"
       ]    
     },
     {
       name: "Innovative Medicine",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+      description: `
+      Collaborated with multiple front-end developers to rebrand a website for a subsidiary of 
+      Johnson & Johnson. Implemented multiple homepage modules, image carousels, page leads, and 
+      article layouts. Developed visually appealing animations, including panel transitions and 
+      fade-ins.`,
       year: "2024",
       link: "https://innovativemedicine.jnj.com/",
       images: [
@@ -51,7 +55,10 @@ export class WorkComponent {
     },
     {
       name: "My Health Can't Wait",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+      description: `
+      Assisted with the rebranding of a Johnson & Johnson informational website by developing key 
+      components. Collaborated with the design team to create buttons, homepage modules, video 
+      carousels, quote carousels, accordion modules, and icon lists.`,
       year: "2023 - 2024",
       link: "https://www.myhealthcantwait.com/",
       images: [
@@ -60,7 +67,11 @@ export class WorkComponent {
     },
     {
       name: "Kenvue",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+      description: `
+      Provided front-end maintenance for a Johnson & Johnson subsidiary website, implementing key 
+      updates to enhance functionality. Developed and updated homepage modules and footer, as well 
+      as overhauled site typography with a new font style. Extensively optimized images for 
+      responsiveness across devices, and ensured all updates met accessibility standards`,
       year: "2023 - 2024",
       link: "https://www.kenvue.com/",
       images: [
@@ -80,7 +91,12 @@ export class WorkComponent {
     },
     {
       name: "Lolita's Tip Tool",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+      description: 
+      `Worked closely with a local restaurant to automate the calculation process of their worker's 
+      daily tip payout, reducing the time for this process from an hour to a few minutes each day. 
+      Results are accessible at any time, downloadable in PDF format, and daily results can be 
+      consolidated across multiple days for monthly payroll processing. Hosted on Netlify using HTML, 
+      JS, CSS, and a Google Firebase Database.`,
       year: "2022 - 2024",
       images: [
         "/images/calc/calc-01.webp", "/images/calc/calc-02.webp", "/images/calc/calc-03.webp", "/images/calc/calc-04.webp" 
@@ -88,7 +104,11 @@ export class WorkComponent {
     },
     {
       name: "Lolita's Invoice Tool",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+      description: 
+      `Developed a custom tool for a local restaurant to streamline daily invoice management. The tool 
+      allows staff to submit invoices, view daily totals, combine invoices to see total cost 
+      across multiple days, and download results in PDF format. Hosted on Netlify using HTML, 
+      JS, CSS, and a Google Firebase Database.`,
       year: "2022 - 2023",
       images: [
         "/images/invoices/invoices-01.webp", "/images/invoices/invoices-02.webp", "/images/invoices/invoices-03.webp"
